6a8f7859e0130464f50184a9a6d255d3fbdcff10
Harold Martínez <harol.lady@hotmail.com>
2023-12-21 09:37:34 -0500

    Registro realizados sin dise;o de interfaz y dise;o de menu tabbed page

--- Reportalo/Views/LoginPage.xaml.cs
+++ Reportalo/Views/LoginPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,11 +16,33 @@ namespace Reportalo.Views
 		public LoginPage ()
 		{
 			InitializeComponent ();
-		}
+            		}
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+
 			Navigation.PushAsync (new RegisterPage ());
+        }
+                
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            var conexion = new MySqlConnection(Properties.Resources.Conexion);
+            conexion.Open();
+
+            var cmd = new MySqlCommand("select * from users where email='" + txtuser.Text + "'and password='" + txtpass.Text + "'",conexion);
+            var rd= cmd.ExecuteReader ();
+
+            if(rd.Read())
+            {
+                DisplayAlert("Aviso", "Informacion Correcta", "Ok");
+                Navigation.PushAsync(new Menu());
+            }
+            else
+            {
+                DisplayAlert("Aviso", "Usuario no Registrado", "Ok");
+            }
+
+            
         }
     }
 }
---
+++ Reportalo/Views/Principal.xaml.cs
@@ -0,0 +1,59 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Reportalo.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Principal : ContentPage
+	{
+		public IList<listadia> listadias { get; set; }
+		public Principal ()
+		{
+			InitializeComponent ();
+			data_list();
+		}
+
+		public class listadia
+		{
+			public string carro_id { get; set; }
+			public string ruta_id { get; set; }
+			public string created_at { get; set; }
+		}
+
+		private void data_list()
+		{
+			var conexion = new MySqlConnection(Properties.Resources.Conexion);
+			conexion.Open ();
+			var cmd = new MySqlCommand("select * from registros", conexion);
+			var rd = cmd.ExecuteReader ();
+
+			listadias = new List<listadia> ();
+
+			while (rd.Read ())
+			{
+				listadias.Add( new listadia
+				{
+                    //created_at = rd.GetDateTime("created_at").ToString(),
+					carro_id = rd.GetInt16("carro_id").ToString(),
+					ruta_id = rd.GetInt16("ruta_id").ToString()
+
+				}
+				);
+			}
+			rd.Close ();
+            vistadia.ItemsSource = listadias;
+		}
+
+        private void vistadia_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            Navigation.PushAsync(new Registros());
+        }
+    }
+}
